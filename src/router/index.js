58392c4345f200c25f60427c29d1ec74a66c9e2e
chuanquan.zheng <240351123@qq.com>
2021-04-07 23:55:03 +0800

    关闭eslint代码检测

--- src/router/index.js
+++ src/router/index.js
@@ -5,23 +5,33 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+	{
+		path: '/',
+		name: 'Home',
+		component: Home,
+		meta: {
+			title: "电子邀请函" //配置title
+		}
+	},
+	{
+		path: '/about',
+		name: 'About',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+	}
 ]
 
 const router = new VueRouter({
-  routes
+	routes
+})
+
+router.beforeEach((to, from, next) => {
+	if (to.meta.title) {
+		document.title = to.meta.title
+	}
+	next()
 })
 
 export default router
